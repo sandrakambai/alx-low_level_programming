0aa79a1d3c3c603cb969b69400de6df1ac5b6283
Sandrakambai <sandrachagusia@gmail.com>
2023-06-15 13:16:39 -0700

    programmimg is like building multilingual puzzles

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -14,8 +14,8 @@ int main(void)
 
 {
 
-puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+        return (0);
 
 }
