4adc64ab8a90fddeb3cb97620e093621d1e6901c
Sandrakambai <sandrachagusia@gmail.com>
2023-06-15 13:06:44 -0700

    C program that prints out C Programmimg is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,20 +2,19 @@
 
 /**
 
- *main - Entry point
+ * main - Entry point
 
  *
- 
+
  * Return: Always 0 (Success)
- 
+
  */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle"); 
+puts("\"Programming is like building a multilingual puzzle");
 
-	return (0)
-
+return (0);
 }
