4f5915fb61f8039b1329d7768c3402bdbe185320
Sandrakambai <sandrachagusia@gmail.com>
2023-06-20 14:07:56 -0700

    spell putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
-	/**
-	 * main - prints _putchar followed by a new line
-	 *
-	 *Description: This function uses the putchar function to print each character of the word"_putchar" followed by a new line.
+/**
+ * main - prints _putchar followed by a new line
+ * is written by mbah
+ * Return: 0
+ */
 
-	 * Return: 0
-	 */
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
